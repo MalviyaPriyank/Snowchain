96c11a831499cc5b7334eeb5b85d2c1ed8afbf9c
root <root@492ea8a02696>
2025-09-17 15:17:01 +0000

    Modify Schema ENSEMBLE_MODEL by PEHLAADMI

---
+++ MY_MODEL_DB/ENSEMBLE_MODEL.sql
@@ -0,0 +1,1 @@
+  CREATE OR REPLACE SCHEMA MY_MODEL_DB.ENSEMBLE_MODEL  REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for storing ensemble model configurations, parameters, and related metadata for machine learning models" 
