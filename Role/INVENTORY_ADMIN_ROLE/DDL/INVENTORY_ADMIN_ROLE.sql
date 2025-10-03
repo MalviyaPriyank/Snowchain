ea099f7a77520e265518450c56ac1c315a74839c
root <root@44459b30def3>
2025-10-03 01:50:35 +0000

    Modify Role INVENTORY_ADMIN_ROLE by PEHLAADMI

--- Role/INVENTORY_ADMIN_ROLE/DDL/INVENTORY_ADMIN_ROLE.sql
+++ Role/INVENTORY_ADMIN_ROLE/DDL/INVENTORY_ADMIN_ROLE.sql
@@ -1,1 +1,1 @@
- CREATE ROLE INVENTORY_ADMIN_ROLE  COMMENT = 'Admin role for Inventory and Products databases' 
+ CREATE ROLE INVENTORY_ADMIN_ROLE  COMMENT = 'Admin role for Inventory and Products related databases' 
