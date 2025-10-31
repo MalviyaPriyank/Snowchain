f88d75f5fdaf817d786d0f0088b5ed30fca018a0
root <root@174feb96e965>
2025-10-31 15:53:53 +0000

    Modify Schema core_banking by FROSTY

--- Database/finance_dev/Schemas/DDL/core_banking.sql
+++ Database/finance_dev/Schemas/DDL/core_banking.sql
@@ -1,1 +1,1 @@
-  CREATE OR REPLACE SCHEMA finance_dev.core_banking  REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for core banking operations and transactions" 
+  CREATE OR REPLACE SCHEMA finance_dev.core_banking  REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for core banking operations including transactions, accounts, and related banking data" 
