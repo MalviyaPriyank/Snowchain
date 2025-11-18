c48694c3096c7b8af2affed7412903d9866f9f15
root <root@ed2c812c1bfe>
2025-11-18 13:46:30 +0000

    Modify ExternalVolume gcs_vol by FROSTY

---
+++ ExternalVolume/gcs_vol/DDL/gcs_vol.sql
@@ -0,0 +1,9 @@
+
+        CREATE EXTERNAL VOLUME gcs_vol 
+        STORAGE_LOCATIONS = (
+            (
+                NAME = 'my-us-east-1'
+                STORAGE_PROVIDER = 'GCS'
+        
+            STORAGE_BASE_URL = 'gcs://mybucket1/path1/'
+             ENCRYPTION = ( TYPE = 'GCS_SSE_KMS'  KMS_KEY_ID = '1234abcd-12ab-34cd-56ef-1234567890ab' )  ))  COMMENT = 'External volume for GCS bucket with KMS encryption' 
