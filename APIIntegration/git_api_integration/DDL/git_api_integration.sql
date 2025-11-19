8967ca952facacf1c9f054f2d79aae72421f10ac
root <root@353edcf41372>
2025-11-19 13:32:58 +0000

    Modify APIIntegration git_api_integration by FROSTY

---
+++ APIIntegration/git_api_integration/DDL/git_api_integration.sql
@@ -0,0 +1,7 @@
+   
+        CREATE API INTEGRATION 
+        git_api_integration
+        API_PROVIDER = git_https_api
+        
+            API_ALLOWED_PREFIXES = ('https://github.com')
+             ALLOWED_AUTHENTICATION_SECRETS = ALL  ENABLED = TRUE  COMMENT = 'NONE' 
