2a3e70d4a033d7dc97471f0e09bafe108678e317
root <root@49045e16f344>
2025-11-25 18:18:59 +0000

    Modify Schema REGISTRATION by FROSTY

---
+++ Database/GEORGIA_TECH_DB/Schemas/DDL/REGISTRATION.sql
@@ -0,0 +1,4 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        GEORGIA_TECH_DB.REGISTRATION 
+         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for storing student registration and related information" 
