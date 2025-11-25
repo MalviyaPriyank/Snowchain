7cb3061547cda36bd3b384107c35471fb88ef93f
root <root@c5e32ba917ab>
2025-11-25 11:29:59 +0000

    Modify ExternalVolume test_ext_vol by FROSTY

---
+++ ExternalVolume/test_ext_vol/DDL/test_ext_vol.sql
@@ -0,0 +1,10 @@
+
+        CREATE EXTERNAL VOLUME test_ext_vol 
+        STORAGE_LOCATIONS = (
+            (
+                NAME = 'my-s3-us-west-2'
+                STORAGE_PROVIDER = 'S3'
+        
+            STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::123456789012:role/myrole'
+            STORAGE_BASE_URL = 's3://my-example-bucket/'
+             ENCRYPTION = ( TYPE = 'AWS_SSE_KMS'  KMS_KEY_ID = '1234abcd-12ab-34cd-56ef-1234567890ab' )  ))  COMMENT = External_volume_with_KMS_encryption 
