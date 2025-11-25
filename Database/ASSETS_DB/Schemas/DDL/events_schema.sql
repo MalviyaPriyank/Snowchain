272f17a764faa275b2affb8b8de632b758f64214
root <root@f2d35d5bb9b9>
2025-11-25 22:13:08 +0000

    Modify Schema events_schema by FROSTY

---
+++ Database/ASSETS_DB/Schemas/DDL/events_schema.sql
@@ -0,0 +1,4 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        assets_db.events_schema 
+         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for storing event-related tables and objects" 
