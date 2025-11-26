adae8318693b58d2fa6202d0f0efa7e397123cb5
root <root@952c109b8107>
2025-11-26 16:02:06 +0000

    Modify Schema incr_schema by FROSTY

---
+++ Database/DB_SP/Schemas/NONE/Schema/incr_schema.sql
@@ -0,0 +1,4 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        db_sp.incr_schema 
+         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Incremental schema for storing delta/changed data for business tables" 
