b57e3273d063b54531fbe43218515d22afc8cd88
root <root@8340514535e0>
2025-11-27 13:34:10 +0000

    Modify Table products by FROSTY

---
+++ Database/COMMERCE_BRONZE/Schemas/COMMERCE_RAW/Table/products.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE TABLE commerce_bronze.commerce_raw.products ( product_id VARCHAR,  product_name VARCHAR,  category_id VARCHAR,  unit_price DECIMAL(10,2),  stock_quantity INTEGER  ) 
