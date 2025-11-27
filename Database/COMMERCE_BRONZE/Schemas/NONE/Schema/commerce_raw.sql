54265ad53b9722b84051a8f2f73d34a02ef297ff
root <root@8340514535e0>
2025-11-27 13:33:44 +0000

    Modify Schema commerce_raw by FROSTY

---
+++ Database/COMMERCE_BRONZE/Schemas/NONE/Schema/commerce_raw.sql
@@ -0,0 +1,4 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        commerce_bronze.commerce_raw 
+         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for raw commerce data including customers and products tables" 
