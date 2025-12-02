c91e0c5e7c6f02986e8d9d84016ab39cfa70b016
root <root@da7de69b4f91>
2025-12-02 14:47:31 +0000

    Modify ExternalStage PRODUCTS_STAGE by FROSTY

---
+++ Database/DEV_REAL/Schemas/COMMERCE/ExternalStage/PRODUCTS_STAGE.sql
@@ -0,0 +1,1 @@
+    CREATE OR REPLACE STAGE DEV_REAL.COMMERCE.PRODUCTS_STAGE  FILE_FORMAT = DEV_REAL.COMMERCE.CSV_FORMAT  COMMENT = 'External stage for products data'  URL = 's3://sf-bucket-snowchain/products/'  STORAGE_INTEGRATION = S3_INT 
