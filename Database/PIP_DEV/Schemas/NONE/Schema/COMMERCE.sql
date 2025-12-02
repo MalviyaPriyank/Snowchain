02eb462f6588fefd7e572be68dca503c1ca7d615
root <root@577be885824c>
2025-12-02 13:56:22 +0000

    Modify Schema COMMERCE by FROSTY

---
+++ Database/PIP_DEV/Schemas/NONE/Schema/COMMERCE.sql
@@ -0,0 +1,4 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        PIP_DEV.COMMERCE 
+         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for commerce data including customers, products, and categories" 
