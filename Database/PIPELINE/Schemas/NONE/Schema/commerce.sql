751acf99bf101dfc2f4c8f9aa93f85a9b7e4fee3
root <root@b838f75e2a2f>
2025-12-02 13:17:52 +0000

    Modify Schema commerce by FROSTY

---
+++ Database/PIPELINE/Schemas/NONE/Schema/commerce.sql
@@ -0,0 +1,4 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        pipeline.commerce 
+         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for e-commerce data including customers, products, and categories" 
