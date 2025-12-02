590371999859e22be601d5afbadf5d9072ae32fb
root <root@04f0144f5d44>
2025-12-02 14:23:05 +0000

    Modify Schema COMMERCE by FROSTY

---
+++ Database/DEV_DB_ING/Schemas/NONE/Schema/COMMERCE.sql
@@ -0,0 +1,4 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        DEV_DB_ING.COMMERCE 
+         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for storing commerce-related tables including customers, products, and categories" 
