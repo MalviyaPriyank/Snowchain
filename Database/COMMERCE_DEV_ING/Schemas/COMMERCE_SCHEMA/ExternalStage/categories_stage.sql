2b742e121f7feedc771e8f20a78e268b61c2e6db
root <root@c914776886f4>
2025-12-03 12:58:35 +0000

    Modify ExternalStage categories_stage by FROSTY

---
+++ Database/COMMERCE_DEV_ING/Schemas/COMMERCE_SCHEMA/ExternalStage/categories_stage.sql
@@ -0,0 +1,1 @@
+    CREATE OR REPLACE STAGE commerce_dev_ing.commerce_schema.categories_stage  FILE_FORMAT = commerce_dev_ing.commerce_schema.commerce_csv_format  COMMENT = 'External stage for categories data'  URL = 's3://sf-bucket-snowchain/categories/'  STORAGE_INTEGRATION = S3_INT 
