0fd292c30df72a2ba0ba882ce75618493d0ca0c6
root <root@76c03e87f29a>
2025-12-10 19:04:24 +0000

    Modify Table PRODUCTS by FROSTY

---
+++ Database/COMMERCE_DEV/Schemas/COMMERCE_DATA_INCREMENTAL/Table/PRODUCTS.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE TABLE COMMERCE_DEV.COMMERCE_DATA_INCREMENTAL.PRODUCTS ( product_id VARCHAR(50),  product_name VARCHAR(255),  category_id VARCHAR(50),  unit_price DECIMAL(10,2),  stock_quantity INTEGER  ) 
