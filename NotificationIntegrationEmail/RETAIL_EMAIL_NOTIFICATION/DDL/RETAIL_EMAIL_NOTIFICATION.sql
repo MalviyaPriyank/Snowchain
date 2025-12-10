7775f8c70be30cccd65823ec11042a0513808b0c
root <root@76c03e87f29a>
2025-12-10 20:07:20 +0000

    Modify NotificationIntegrationEmail RETAIL_EMAIL_NOTIFICATION by FROSTY

---
+++ NotificationIntegrationEmail/RETAIL_EMAIL_NOTIFICATION/DDL/RETAIL_EMAIL_NOTIFICATION.sql
@@ -0,0 +1,3 @@
+   CREATE NOTIFICATION INTEGRATION  RETAIL_EMAIL_NOTIFICATION TYPE = EMAIL ENABLED = TRUE  ALLOWED_RECIPIENTS = ('priyank@thegyrus.com') 
+ DEFAULT_SUBJECT = 'Retail Data Load Task Failure' 
+ COMMENT = 'Email notification for retail data load task failures' 
