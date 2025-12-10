b0dab99816af56ba162e3d5a33cf1ad4cbf26907
root <root@76c03e87f29a>
2025-12-10 19:43:25 +0000

    Modify Schema SALES_MODELS by FROSTY

---
+++ Database/SALES_ANALYTICS_DB/Schemas/NONE/Schema/SALES_MODELS.sql
@@ -0,0 +1,5 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        SALES_ANALYTICS_DB.SALES_MODELS 
+         REPLACE_INVALID_CHARACTERS = FALSE 
+ COMMENT = "Schema for sales analytics team data models and derived tables" 
