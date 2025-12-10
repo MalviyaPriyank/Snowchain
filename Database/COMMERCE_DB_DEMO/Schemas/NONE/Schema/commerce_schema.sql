c5d5430ff6ae5293a3013ae4a2966ab72c1d4078
root <root@76c03e87f29a>
2025-12-10 19:12:57 +0000

    Modify Schema commerce_schema by FROSTY

---
+++ Database/COMMERCE_DB_DEMO/Schemas/NONE/Schema/commerce_schema.sql
@@ -0,0 +1,5 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        commerce_db_demo.commerce_schema 
+         REPLACE_INVALID_CHARACTERS = FALSE 
+ COMMENT = "Schema for commerce application source tables" 
