e3d8e120e83a429011d9f2f07250985710907450
root <root@76c03e87f29a>
2025-12-10 19:37:16 +0000

    Modify Schema ANALYTICS_MODELS by FROSTY

---
+++ Database/ANALYTICS_WORKSPACE_DB/Schemas/NONE/Schema/ANALYTICS_MODELS.sql
@@ -0,0 +1,5 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        ANALYTICS_WORKSPACE_DB.ANALYTICS_MODELS 
+         REPLACE_INVALID_CHARACTERS = FALSE 
+ COMMENT = "Schema for analytics team data models, views, and derived tables" 
