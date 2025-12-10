ff6ea023284b2c7de9abff8446de33118d308158
root <root@76c03e87f29a>
2025-12-10 19:03:35 +0000

    Modify Schema COMMERCE_DATA_INCREMENTAL by FROSTY

---
+++ Database/COMMERCE_DEV/Schemas/NONE/Schema/COMMERCE_DATA_INCREMENTAL.sql
@@ -0,0 +1,5 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        COMMERCE_DEV.COMMERCE_DATA_INCREMENTAL 
+         REPLACE_INVALID_CHARACTERS = FALSE 
+ COMMENT = "Schema for incremental commerce data loads" 
