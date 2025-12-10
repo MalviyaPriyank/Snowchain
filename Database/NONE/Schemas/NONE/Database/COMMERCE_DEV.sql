7765afe224029944b83d20da88c75c7cfcfb997a
root <root@f258128b94d0>
2025-12-10 18:53:29 +0000

    Modify Database COMMERCE_DEV by FROSTY

--- Database/NONE/Schemas/NONE/Database/COMMERCE_DEV.sql
+++ Database/NONE/Schemas/NONE/Database/COMMERCE_DEV.sql
@@ -1,4 +1,4 @@
  
         CREATE OR REPLACE DATABASE  
         COMMERCE_DEV 
-         COMMENT = 'Database for commerce development' 
+         COMMENT = 'Database for commerce development environment' 
