ee724ef450f30cb66b016232409476c72cc46b3e
root <root@76c03e87f29a>
2025-12-10 19:03:26 +0000

    Modify Schema COMMERCE_DATA by FROSTY

--- Database/COMMERCE_DEV/Schemas/NONE/Schema/COMMERCE_DATA.sql
+++ Database/COMMERCE_DEV/Schemas/NONE/Schema/COMMERCE_DATA.sql
@@ -1,4 +1,5 @@
   
         CREATE OR REPLACE SCHEMA 
         COMMERCE_DEV.COMMERCE_DATA 
-         REPLACE_INVALID_CHARACTERS = FALSE  COMMENT = "Schema for storing original commerce data" 
+         REPLACE_INVALID_CHARACTERS = FALSE 
+ COMMENT = "Schema for main commerce data including customers, products, and categories" 
