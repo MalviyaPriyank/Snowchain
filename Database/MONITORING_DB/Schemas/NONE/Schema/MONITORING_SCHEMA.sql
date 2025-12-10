eabe09894df6f6d4dca469c3672f66ea56e8d0d6
root <root@f81d25011cb4>
2025-12-10 23:17:45 +0000

    Modify Schema MONITORING_SCHEMA by FROSTY

---
+++ Database/MONITORING_DB/Schemas/NONE/Schema/MONITORING_SCHEMA.sql
@@ -0,0 +1,5 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        MONITORING_DB.MONITORING_SCHEMA 
+         REPLACE_INVALID_CHARACTERS = FALSE 
+ COMMENT = "Schema for monitoring and alerting configurations" 
