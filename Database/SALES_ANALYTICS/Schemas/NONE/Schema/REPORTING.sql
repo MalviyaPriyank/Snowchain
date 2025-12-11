8a2ebb1387a23ee61c16319cf36a76a111f31d51
root <root@78f16426ab26>
2025-12-11 02:12:47 +0000

    Modify Schema REPORTING by FROSTY

---
+++ Database/SALES_ANALYTICS/Schemas/NONE/Schema/REPORTING.sql
@@ -0,0 +1,5 @@
+  
+        CREATE OR REPLACE SCHEMA 
+        SALES_ANALYTICS.REPORTING 
+         REPLACE_INVALID_CHARACTERS = FALSE 
+ COMMENT = "Schema for sales analytics reporting and dashboards" 
